2ec161f34a996ed4290efd92dd8dbd14c8866365
Maximiliano Jabase <maxijabase@gmail.com>
2022-12-29 14:33:31 -0300

    replace gamedig

--- src/routes/api/servers/+server.ts
+++ src/routes/api/servers/+server.ts
@@ -1,17 +1,18 @@
 import { json } from '@sveltejs/kit';
-import Gamedig from 'gamedig';
+import { Server } from '@fabricio-191/valve-server-query'
 import type { RequestHandler } from '../$types';
 
 export const GET: RequestHandler = async () => {
-	let result = await Gamedig.query({
-		type: 'tf2',
-		host: '45.235.99.105',
+	const server = await Server({
+		ip: '45.235.99.105',
 		port: 27034
 	});
 
+	const svInfo = await server.getInfo();
+
 	return json({ 
-		name: result.name, 
-		map: result.map, 
-		connect: result.connect 
+		name: svInfo.name, 
+		map: svInfo.map, 
+		connect: svInfo.address 
 	});
 };
