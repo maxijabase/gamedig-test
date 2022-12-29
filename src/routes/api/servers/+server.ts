d7877170c2ca4dcc54af6523bb20acb64e038105
Maximiliano Jabase <maxijabase@gmail.com>
2022-12-29 18:51:33 -0300

    Revert "replace gamedig"
    This reverts commit 2ec161f34a996ed4290efd92dd8dbd14c8866365.

--- src/routes/api/servers/+server.ts
+++ src/routes/api/servers/+server.ts
@@ -1,18 +1,17 @@
 import { json } from '@sveltejs/kit';
-import { Server } from '@fabricio-191/valve-server-query'
+import Gamedig from 'gamedig';
 import type { RequestHandler } from '../$types';
 
 export const GET: RequestHandler = async () => {
-	const server = await Server({
-		ip: '45.235.99.105',
+	let result = await Gamedig.query({
+		type: 'tf2',
+		host: '45.235.99.105',
 		port: 27034
 	});
 
-	const svInfo = await server.getInfo();
-
 	return json({ 
-		name: svInfo.name, 
-		map: svInfo.map, 
-		connect: svInfo.address 
+		name: result.name, 
+		map: result.map, 
+		connect: result.connect 
 	});
 };
